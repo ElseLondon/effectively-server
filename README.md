4e414f49842c2897974ca950080589e7c57b10f8
sim-ware <samir.gossain@gmail.com>
2022-05-10 17:20:23 +0100

    Initial commit - server deployed to App Engine and running as expected

---
+++ README.md
@@ -0,0 +1,5 @@
+explanation for why this server is needed
+
+gcloud deployment instructions (gcloud app deploy)
+
+simple DB?
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+const jsonParser = bodyParser.json();
+
+app.use(cors());
+
+const meetingRoomAgendaDetails = {};
+
+app.use('/setRoomAgenda', jsonParser, (req, res) => {
+  console.log('req.body', JSON.stringify(req.body));
+
+  const roomName = Object.keys(req.body)[0];
+  meetingRoomAgendaDetails[roomName] = req.body[roomName];
+
+  res.status(200).send(JSON.stringify(meetingRoomAgendaDetails));
+});
+
+app.use('/getRoomAgenda', jsonParser, (req, res) => {
+  console.log('req.body', JSON.stringify(req.body));
+
+  res.status(200).send(meetingRoomAgendaDetails);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
