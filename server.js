149e5c5aa611468fdc2a198a95f905081edd8ea7
sim-ware <samir.gossain@gmail.com>
2022-05-29 19:47:49 +0100

    Adds logging for start timer route

--- server.js
+++ server.js
@@ -26,12 +26,15 @@ app.use('/getRoomAgenda', jsonParser, (req, res) => {
 });
 
 app.use('/startMeetingTimer', jsonParser, (req, res) => {
-  console.log('req.body', JSON.stringify(req.body));
-  
-  const roomName = Object.keys(req.body)[0];
+  const roomName = req.body["room_name"];
+  console.log('/startMeetingTimer_running...-------------------------------------------------------------------------');
+  console.log('req.body',                                           JSON.stringify(req.body));
+  console.log('roomName',                                           roomName);
+  console.log('meetingRoomAgendaDetails',                           meetingRoomAgendaDetails);
+  console.log('meetingRoomAgendaDetails[roomName]',                 meetingRoomAgendaDetails[roomName]);
+  console.log('meetingRoomAgendaDetails[roomName].meeting_started', meetingRoomAgendaDetails[roomName].meeting_started);
   meetingRoomAgendaDetails[roomName].meeting_started = true;
-
-  res.status(200).send(JSON.stringify(meetingRoomAgendaDetails));
+  res.status(200);
 });
 
 app.listen(PORT, () => {
