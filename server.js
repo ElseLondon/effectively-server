2abd1132d9201ecee5678508b3ff6d11f189a7e4
sim-ware <samir.gossain@gmail.com>
2022-05-30 11:07:31 +0100

    Reduces logging for startMeetingTimer and adds route name to other logging

--- server.js
+++ server.js
@@ -11,28 +11,26 @@ app.use(cors());
 const meetingRoomAgendaDetails = {};
 
 app.use('/setRoomAgenda', jsonParser, (req, res) => {
+  console.log('/setRoomAgenda_running...');
   console.log('req.body', JSON.stringify(req.body));
 
   const roomName = Object.keys(req.body)[0];
   meetingRoomAgendaDetails[roomName] = req.body[roomName];
-
   res.status(200).send(JSON.stringify(meetingRoomAgendaDetails));
 });
 
 app.use('/getRoomAgenda', jsonParser, (req, res) => {
+  console.log('/getRoomAgenda_running...');
   console.log('req.body', JSON.stringify(req.body));
 
   res.status(200).send(meetingRoomAgendaDetails);
 });
 
 app.use('/startMeetingTimer', jsonParser, (req, res) => {
-  const roomName = req.body["room_name"];
-  console.log('/startMeetingTimer_running...-------------------------------------------------------------------------');
-  console.log('req.body',                                           JSON.stringify(req.body));
-  console.log('roomName',                                           roomName);
-  console.log('meetingRoomAgendaDetails',                           meetingRoomAgendaDetails);
-  console.log('meetingRoomAgendaDetails[roomName]',                 meetingRoomAgendaDetails[roomName]);
-  console.log('meetingRoomAgendaDetails[roomName].meeting_started', meetingRoomAgendaDetails[roomName].meeting_started);
+  console.log('/startMeetingTimer_running...');
+  console.log('req.body', JSON.stringify(req.body));
+
+  const roomName = req.body["room_name"]
   meetingRoomAgendaDetails[roomName].meeting_started = true;
   res.status(200);
 });
