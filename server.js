a6a2ff747b9f900938ed1c270114cd1179280202
Samir Gossain <samir.gossain@gmail.com>
2022-05-30 14:58:31 +0100

    Meeting Timer |  `startMeetingTimer` & `checkMeetingTimerStatus` (#1)
    * Scaffold new routes for starting and checking meeting timer
    
    * Removes endpoint to check for whether it's started, will just reuse getAgendaDetails
    
    * Returns meeting details as payload within startMeetingTimer
    
    * Adds logging for start timer route
    
    * Reduces logging for startMeetingTimer and adds route name to other logging

--- server.js
+++ server.js
@@ -11,18 +11,28 @@ app.use(cors());
 const meetingRoomAgendaDetails = {};
 
 app.use('/setRoomAgenda', jsonParser, (req, res) => {
+  console.log('/setRoomAgenda_running...');
   console.log('req.body', JSON.stringify(req.body));
 
   const roomName = Object.keys(req.body)[0];
   meetingRoomAgendaDetails[roomName] = req.body[roomName];
-
   res.status(200).send(JSON.stringify(meetingRoomAgendaDetails));
 });
 
 app.use('/getRoomAgenda', jsonParser, (req, res) => {
+  console.log('/getRoomAgenda_running...');
   console.log('req.body', JSON.stringify(req.body));
 
   res.status(200).send(meetingRoomAgendaDetails);
+});
+
+app.use('/startMeetingTimer', jsonParser, (req, res) => {
+  console.log('/startMeetingTimer_running...');
+  console.log('req.body', JSON.stringify(req.body));
+
+  const roomName = req.body["room_name"]
+  meetingRoomAgendaDetails[roomName].meeting_started = true;
+  res.status(200);
 });
 
 app.listen(PORT, () => {
